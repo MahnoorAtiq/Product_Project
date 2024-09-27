9efd52a02ccb73eef8369ed6b20670c17cac1607
MahnoorAtiq <mahnoor.atique72@gmail.com>
2024-09-27 22:48:07 +0500

    Created main file

---
+++ products_stubs.py
@@ -0,0 +1,13 @@
+#Maintainer's Reg No: SP23-BAI-023
+# Collaborator's Reg No: SP23-BAI-016
+class Product:
+  def _init_(self, name, amount, price):
+      self.name=name
+      self.amount=amount
+      self.price=price
+
+  def get_price(self, quantity):
+      pass
+
+  def make_purchase(self, quantity):
+     pass
